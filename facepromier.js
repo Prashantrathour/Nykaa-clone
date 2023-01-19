bfa033ef94fc42210d798ef0b23294639a3e31f6
prashant_rathour <prathour884@gmail.com>
2023-01-19 22:32:34 +0530

    day4

--- facepromier.js
+++ facepromier.js
@@ -1,26 +1,11 @@
-console.log("hello")
-imageslide=["https://images-static.nykaa.com/uploads/ea7f773b-1b3d-46e8-bb13-5c5f075b789b.jpg?tr=w-1200,cm-pad_resize",
-"https://images-static.nykaa.com/uploads/32e8d407-f9b5-485a-b953-a25d379dcc80.jpg?tr=w-1200,cm-pad_resize",
-"https://images-static.nykaa.com/uploads/9813e7b1-6186-4189-a04d-e7de75104e4b.jpg?tr=w-1200,cm-pad_resize",
-"https://images-static.nykaa.com/uploads/9ef6a6c2-9107-487c-8a4b-e886becea619.jpg?tr=w-1200,cm-pad_resize",
-"https://images-static.nykaa.com/uploads/04d0c1d0-3bd5-49cd-8d38-655a9f3c003b.jpg?tr=w-1200,cm-pad_resize",
-"https://images-static.nykaa.com/uploads/a294f8d2-8bb6-4912-9ac9-896cfa2685dc.jpg?tr=w-1200,cm-pad_resize",
-"https://images-static.nykaa.com/uploads/722e2d8c-476e-4993-be69-a505143ceca2.jpg?tr=w-1200,cm-pad_resize",
-"https://images-static.nykaa.com/uploads/4e0cc868-6e20-4dd9-9200-3e5dfa660a88.jpg?tr=w-1200,cm-pad_resize",
-"https://images-static.nykaa.com/uploads/a908e8ad-3bd3-4d87-bca2-2fca7c54bf98.jpg?tr=w-1200,cm-pad_resize"]
-let count=0;
-let slide=document.getElementById("slide");
-img=document.createElement("img")
-slide.append(img)
 
-setInterval(() => {
-  if(count==imageslide.length){
-    count=0
-  }
-  img.setAttribute("src",imageslide[count])
-  count++;
-}, 1500);
-var data = [
+let home=document.getElementById("home")
+home.addEventListener("click",(e)=>{
+      e.preventDefault()
+      window.location.href="./index.html"
+})
+
+var data1= [
     //   makeup revolution / face / face primer
     {
       brand: "Makeup Revolution",
@@ -31,6 +16,7 @@ var data = [
         "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/f/2/f200c585057566572804_1.jpg",
       str_price: "812",
       price: "750",
+      id:1
     },
     {
       brand: "Makeup Revolution",
@@ -41,6 +27,7 @@ var data = [
         "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/0/4/04bcb845057566296502_-_1.jpg",
       str_price: "699",
       price: "629",
+      id:2
     },
     {
       brand: "Makeup Revolution",
@@ -51,6 +38,7 @@ var data = [
         "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/f/5/f5ef6f55057566076579_1.jpg",
       str_price: "850",
       price: "723",
+
     },
     {
       brand: "Makeup Revolution",
@@ -315,35 +303,160 @@ var data = [
       price: "956",
     },
   ];
-  console.log("data")
-console.log(data[0].price)
+
+
+let sort=document.getElementById("sort");
+let parent=document.getElementById("cont")
+function sortproduct(data){
+  sort.addEventListener("change",((e)=>{
+    
+    
+    e.preventDefault()
+    if(sort.value=="1"){
+      display(data1)
+     
+    
+      
+
+    }else if(sort.value=="pricelowtohigh"){
+      filtered=data.sort((a,b)=>{
+        return a.price-b.price
+      })
+      display(filtered)
+      
+    }
+    else if(sort.value=="pricehightolow"){
+      filtered=data.sort((a,b)=>{
+        return b.price-a.price
+      })
+      display(filtered)
+      
+    }else if(sort.value=="name"){
+      filtered=data.sort((a,b)=>{if(a.brand >b.brand) return 1 })
+      filtered=data.sort((a,b)=>{if(a.brand <b.brand) return -1 })
+      display(filtered)
+    }
+  }))
+}
+sortproduct(data1)
+let dataarray=JSON.parse(localStorage.getItem("addtocart"))||[]
+  ////
+  console.log(dataarray)
 let container=document.getElementById("cont")
-console.log(  "cont"+container)
+
+
+//////
+let search=document.querySelector("form")
+
+search.addEventListener("submit",(e)=>{
+  e.preventDefault();
+valuesearch=search.search.value;
+let filtered=data1.filter((el)=>{
+  if( el.country.toUpperCase().includes(value.toUpperCase()) ===true){
+     return true 
+  }else{
+      return false
+  }
+ 
+      
+})
+display(filtered)
+console.log(valuesearch)
+})
+
 function display(data) {
+  
+///////check
+
+
+
     container.innerHTML="";
     data.forEach((el,i) => {
-      console.log(el.sub_price)
+    
         let card=document.createElement("div")
         let imge=document.createElement("img")
         imge.setAttribute("src",el.img_url)
-        let h2=document.createElement("h2")
+        let h2=document.createElement("h6")
         h2.innerText=el.prodname
-        let h4=document.createElement("h4");
+        let h4=document.createElement("h6");
         h4.innerText=el.brand
-        let h5=document.createElement("h4");
-        h4.innerText=el.category;
-        let p=document.createElement("p");
-        p.innerText=el.sub_category
-        let h3=document.createElement("h3");
-        h3.innerText=el.price
-        let h3a=document.createElement("h3");
-        h3a.innerText=el.str_price
-card.append(imge,h2,h4,h5,p,h3,h3a)
+        // let h5=document.createElement("h6");
+        // h4.innerText=el.category;
+        
+        let h3=document.createElement("h6");
+        h3.innerText="₹"+el.price
+        let h3a=document.createElement("h6");
+        h3a.innerText="MRP"+" "+"₹"+el.str_price
+        h3a.setAttribute("class","cutt")
+        let p=document.createElement("h6");
+        p.innerText="Enjoy Free Gift"
+        let br=document.createElement("br")
+        br.innerText=""
+        let rat=document.createElement("p")
+        rat.setAttribute("class","fa fa-star fa-1x checked")
+        let rat2=document.createElement("p")
+        rat2.setAttribute("class","fa fa-star fa-1x checked")
+        let rat3=document.createElement("p")
+        rat3.setAttribute("class","fa fa-star fa-1x checked")
+        let rat4=document.createElement("p")
+        rat4.setAttribute("class","fa fa-star fa-1x")
+        let button=document.createElement("button");
+        button.innerText="Add to Card"
+        button.addEventListener("click",(product)=>{
+          if(check(el)){
+            alert("product Already Added")
+          }else{
+            let element=data.splice(el,1)
+            dataarray.push(element[0])
+           localStorage.setItem("addtocart",JSON.stringify(dataarray))
+           
+           alert("product Added")
+          }
+   
+           
+        })
+       
+card.append(imge,h2,h4,h3a,h3,p,rat,rat2,rat3,rat4,button)
 container.append(card)
 
+
     });
+    
+   
+   
+    
+    
 }
-  display(data)
+console.log(dataarray)
+display(data1)
+function check(el){
+  for(let i=0;i<dataarray.length;i++){
+   
+    console.log("check"+dataarray,el)
+    if(dataarray[i].price==el.price){
+     
+      return true
+    }
+    return false
+  }
+}
+
+ 
+
+  imageslide=["https://images-static.nykaa.com/uploads/fe975d47-3792-4599-978f-ee185ef9f114.jpg?tr=w-1200,cm-pad_resize","https://images-static.nykaa.com/uploads/fe975d47-3792-4599-978f-ee185ef9f114.jpg?tr=w-1200,cm-pad_resize","https://images-static.nykaa.com/uploads/67dc1a42-52d5-4f03-b7eb-ce1e35bf6594.jpg?tr=w-1200,cm-pad_resize","https://images-static.nykaa.com/uploads/93c65307-10fd-489a-9fe3-2cf86df3f037.jpg?tr=w-1200,cm-pad_resize","https://images-static.nykaa.com/uploads/93c65307-10fd-489a-9fe3-2cf86df3f037.jpg?tr=w-1200,cm-pad_resize","https://images-static.nykaa.com/uploads/381bc88c-1637-49da-9004-eda6690cf3f1.jpg?tr=w-1200,cm-pad_resize","https://images-static.nykaa.com/uploads/fe975d47-3792-4599-978f-ee185ef9f114.jpg?tr=w-1200,cm-pad_resize",]
+  let count=0;
+  let facebanner=document.getElementById("facebanner");
+  img=document.createElement("img")
+  facebanner.append(img)
+  total=document.querySelector(".count")
+  total.innerText=data1.length
+  setInterval(() => {
+    if(count==imageslide.length){
+      count=0
+    }
+    img.setAttribute("src",imageslide[count])
+    count++;
+  }, 1000);
 
   
   var popular_brand_names = [
